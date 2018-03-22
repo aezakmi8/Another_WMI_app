bf594315534b4656648de34041d2049d7a4b034d
???????°???» ???µ?????????? <mis5919@yandex.ru>
2018-03-22 21:54:31 +0300

    some improvements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,27 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.HardTreeinfo = new System.Windows.Forms.TreeView();
+            this.classList = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.LogBox = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(192, 12);
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 34);
+            this.button1.Size = new System.Drawing.Size(107, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // HardTreeinfo
+            // 
+            this.HardTreeinfo.BackColor = System.Drawing.SystemColors.InfoText;
+            this.HardTreeinfo.ForeColor = System.Drawing.Color.Lime;
+            this.HardTreeinfo.Location = new System.Drawing.Point(127, 13);
+            this.HardTreeinfo.Name = "HardTreeinfo";
+            this.HardTreeinfo.Size = new System.Drawing.Size(310, 308);
+            this.HardTreeinfo.TabIndex = 1;
+            // 
+            // classList
+            // 
+            this.classList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.classList.BackColor = System.Drawing.SystemColors.InfoText;
+            this.classList.ForeColor = System.Drawing.Color.Lime;
+            this.classList.FormattingEnabled = true;
+            this.classList.ItemHeight = 16;
+            this.classList.Location = new System.Drawing.Point(443, 12);
+            this.classList.Name = "classList";
+            this.classList.Size = new System.Drawing.Size(291, 308);
+            this.classList.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 42);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // LogBox
+            // 
+            this.LogBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogBox.BackColor = System.Drawing.SystemColors.MenuText;
+            this.LogBox.ForeColor = System.Drawing.Color.Lime;
+            this.LogBox.FormattingEnabled = true;
+            this.LogBox.ItemHeight = 16;
+            this.LogBox.Location = new System.Drawing.Point(127, 335);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(485, 116);
+            this.LogBox.TabIndex = 8;
+            this.LogBox.SelectedIndexChanged += new System.EventHandler(this.LogBox_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(618, 335);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(128, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(746, 456);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.classList);
+            this.Controls.Add(this.HardTreeinfo);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +142,12 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView HardTreeinfo;
+        private System.Windows.Forms.ListBox classList;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox LogBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Management;
+using System.Management.Instrumentation;
+
+
 
 namespace Another_WMI_app
 {
@@ -17,10 +24,168 @@ namespace Another_WMI_app
             InitializeComponent();
         }
 
+        private void AddClassesToList()
+        {
+            this.LogBox.Text = "Searching...";
+            try
+            {
+                //List<string> HardType = new List<string> { "Win32_Processor", "Win32_VideoController" };
+                WqlObjectQuery query = new WqlObjectQuery("SELECT * FROM meta_class WHERE __class LIKE 'Win32_%' ");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+                int count = 0;
+                foreach (ManagementObject wmiClass in searcher.Get())
+                {
+                    if (count == 74)
+                    {
+                        break;
+                    }
+                    string className = wmiClass.Path.ClassName;
+                    HardTreeinfo.Nodes.Add(className);
+                    //Get_WMI_Class(wmiClass.Path.ClassName, count);
+                    //HardTreeinfo.Nodes[count].Nodes.Add(ListToNodeItem(GetWMIClass(wmiClass.Path.ClassName, count)));
+                    //HardTreeinfo.Nodes[count].Nodes.AddRange(ListToNodeItem(GetWMIClass(wmiClass.Path.ClassName, count)));\ working
+                    TreeNode obj = HardTreeinfo.Nodes[count];
+                    Professional(GetWMIClass(wmiClass.Path.ClassName, count), ref obj);
+                    // classList.Items.Add(wmiClass.Path.ClassName.Trim());  //первые 74
+                    count++;
+                }
+                LogBox.Items.Add(count + " classes found.");
+            }
+            catch (Exception ex)
+            {
+                LogBox.Items.Add(ex.Message);
+            }
+
+        }
+
+        public void Professional(List<string> list, ref TreeNode node)
+        {
+            foreach (var i in list)
+            {
+                node.Nodes.Add(i);
+            }
+        }
+
+
+        private List<string> GetWMIClass(string Win32_Process, int count)
+        {
+            List<string> result = new List<string>();
+            // Get the WMI class
+            ManagementClass processClass =
+                new ManagementClass(Win32_Process);
+            processClass.Options.UseAmendedQualifiers = true;
+            // Get the properties in the class
+            PropertyDataCollection properties =
+                processClass.Properties;
+            // display the properties
+            try
+            {
+                    foreach (PropertyData property in properties)
+                    {
+                        //LogBox.Items.Add(property.Name);    //Получаем имена полей из классов
+                        //HardTreeinfo.Nodes[count].Nodes.Add(property.Name);
+                        //result.Add(property.Name.Trim() + obj[property.Name.Trim()].ToString().Trim());
+                        result.Add(property.Name.Trim());
+                        //foreach (QualifierData q in property.Qualifiers)
+                        //{
+                        //    if (q.Name.Equals("Description")) //Получаем описание
+                        //    {
+                        //        LogBox.Items.Add((string)processClass.GetPropertyQualifierValue(property.Name, q.Name));
+                        //    }
+                        //}
+                    }
+            }
+            catch (Exception ex)
+            {
+                LogBox.Text = ex.Message;
+            }
+            return result;
+        }
+
+        private List <string> GetNodeItems (List<string> list)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + Win32_Process);
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                obj[list[i]].ToString().Trim()
+                }
+            return list;
+        }
+
+        private static object ClassItemField(ManagementObject ClassItem)
+        {
+
+            return ClassItem;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            RemoteConnect.WMI_Conn();
+            //RemoteConnect.WMI_Conn();
+            AddClassesToList();
+        }
+
+        private void ConstructWMIClassTree(System.Windows.Forms.TreeView item)
+        {
+            ManagementScope mgmtScope = new ManagementScope("root/cimv2");
+            ManagementPath mgmtPath = new ManagementPath("__NAMESPACE");
+            using (ManagementClass mgmtClass = new ManagementClass(mgmtScope, mgmtPath, null))
+            {
+                foreach (ManagementObject mgmtObject in mgmtClass.GetInstances())
+                {
+                    string name = (string)mgmtObject.Properties["Name"].Value;
+                    LogBox.Items.Add((string)name);
+                    if (!string.IsNullOrEmpty(name))
+                    {
+                        System.Windows.Forms.TreeView newTreeViewItem = new System.Windows.Forms.TreeView();
+                        newTreeViewItem.Name = name;
+                        LogBox.Items.Add((string)item.Tag);
+                        StringBuilder sb = new StringBuilder(mgmtClass.Path.ToString());
+                        sb.Replace(mgmtClass.Path.ClassName, string.Empty);
+                        sb.Replace(":", string.Empty);
+                        sb.Append(@"\");
+                        sb.Append(name);
+                        newTreeViewItem.Tag = sb.ToString();
+                        //newTreeViewItem.AfterSelect += new RoutedEventHandler(WMIClassesItem_Selected);
+                        LogBox.Items.Add(newTreeViewItem.Name);
+                    }
+
+                }
+            }
+        }
+
+        private void WMIClassesItem_Selected(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.TreeView item = (System.Windows.Forms.TreeView)sender;
+            if (item.Nodes.Count == 0)
+            {
+                ConstructWMIClassTree(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AddClassesToList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
 }
+
